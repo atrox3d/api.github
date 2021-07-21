64853bba91610b29b51e3f073cdf555a65a91340
atrox3d <atrox3d@gmail.com>
2021-07-21 10:49:32 +0200

    add pagination and generalize kwargs

--- helpers/api.py
+++ helpers/api.py
@@ -19,7 +19,7 @@ class GithubApi:
         self.api = api
         self.public_repos = public_repos or TEMPLATE_PUBLIC_REPOS_URL.format(api=api, user=user)
         self.private_repos = private_repos or TEMPLATE_PRIVATE_REPOS_URL.format(api=api)
-        self.response = None
+        # self.response = None
 
     @classmethod
     def from_export(cls, export_path):
@@ -35,24 +35,62 @@ class GithubApi:
         except KeyError as ke:
             raise SystemExit(f"FATAL| {ke}")
 
-    def api_call(self, url, params=None, headers=None):
+    def api_call(self, url, params=None, headers=None, auth=None):
+        # kwargs = (lambda **kargs: kargs)(headers=headers, auth=auth)
+        auth = auth or (self.user, self.token)
+        kwargs = {name: value for name, value in locals().items() if name not in ["self", "url"] and value}
+        # response = requests.get(
+        #     url,
+        #     auth=(self.user, self.token),
+        #     params=params,
+        #     headers=headers
+        # )
         response = requests.get(
             url,
-            auth=(self.user, self.token),
-            params=params,
-            headers=headers
+            **kwargs
         )
         response.raise_for_status()
         self.response = response
         return response
 
-    def get_repos(self, visibility=REPOS_ALL):
-        pass
+    def get_repos(self, visibility=REPOS_ALL, per_page=100):
+        _json = []
+        nextpage = 1
+        while nextpage:
+            params = dict(visibility=visibility, per_page=per_page, page=nextpage)
+            if visibility == REPOS_PUBLIC:
+                response = self.api_call(self.public_repos, params=params)
+            else:
+                response = self.api_call(self.private_repos, params=params)
+            _json.extend(response.json())
+            nextpage = self.nextpage(response)
+        return _json
+
+    @staticmethod
+    def nextpage(response):
+        try:
+            link = response.headers["link"]
+            regex = "([0-9]+)>; rel=\"next\""
+            search = re.search(regex, link)
+            # print(f"{link=}")
+            # print(f"{regex=}")
+            # print(f"{search=}")
+            # print(f"{search.groups()=}")
+            page, = search.groups()
+            # print(f"nextpage: {page}")
+            # exit()
+            return int(page)
+        except (KeyError, AttributeError):
+            # print(f"nextpage: {False}")
+            return False
 
     @staticmethod
     def get_totalpages(response):
-        link = response.headers["link"]
-        regex = "([0-9]+)>; rel=\"last\""
-        search = re.search(regex, link)
-        pages, = search.groups()
-        return int(pages)
+        try:
+            link = response.headers["link"]
+            regex = "([0-9]+)>; rel=\"last\""
+            search = re.search(regex, link)
+            pages, = search.groups()
+            return int(pages)
+        except KeyError:
+            return 1
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import json
+
 from helpers.exports import parse_exports
-from helpers.api import GithubApi
+from helpers import api
 
 try:
     # variables = parse_exports("data/github_export")
     # print(json.dumps(variables, indent=4))
-    gh = GithubApi.from_export("data/github_export")
+    gh = api.GithubApi.from_export("data/github_export")
 except KeyError as ke:
     raise SystemExit(f"FATAL| {ke}")
 
-response = gh.api_call(url=gh.private_repos)
-print(gh.get_totalpages(response))
+data = gh.get_repos(visibility=api.REPOS_ALL)
+print(f"{len(data)}")
